3b3f713079fbad4a4f41040c14bb2940f24bbf65
Jovan Vunic <jovanvunicnovisad@gmail.com>
2021-01-13 13:22:12 +0100

    Initial commit

---
+++ src/main/java/com/wedoqa/qalearning/QalearningApplication.java
@@ -0,0 +1,13 @@
+package com.wedoqa.qalearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QalearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QalearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wedoqa/qalearning/QalearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedoqa.qalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QalearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
